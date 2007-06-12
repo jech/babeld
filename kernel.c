17ef3ef2d5597aea33ca58644476df5c686a62e0
Grégoire Henry <henry@pps.jussieu.fr>
2007-06-12 17:31:07 +0200

    Switch to using netlink for kernel route updates. Still slightly incomplete, we do monitor for route changes, but don't do anything with the result.  Written by Gregoire Henry and Julien Cristau.

--- kernel.c
+++ kernel.c
@@ -26,12 +26,19 @@ THE SOFTWARE.
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <time.h>
 
 #include <sys/ioctl.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <net/route.h>
 #include <net/if.h>
+#include <arpa/inet.h>
+
+#include <asm/types.h>
+#include <sys/socket.h>
+#include <linux/netlink.h>
+#include <linux/rtnetlink.h>
 
 #include "babel.h"
 #include "kernel.h"
@@ -88,12 +95,344 @@ write_proc(char *filename, int value)
     return 1;
 }
 
+struct netlink {
+    unsigned int seqno;
+    int sock;
+    struct sockaddr_nl sockaddr;
+    socklen_t socklen;
+};
+
+static struct netlink nl_command = { 0, -1, {0}, 0 };
+static struct netlink nl_listen = { 0, -1, {0}, 0 };
+
+static int
+netlink_socket(struct netlink *nl, uint32_t groups)
+{
+    int rc;
+
+    nl->sock = socket(PF_NETLINK, SOCK_RAW, NETLINK_ROUTE);
+    if(nl->sock < 0)
+        return -1;
+
+    memset(&nl->sockaddr, 0, sizeof(nl->sockaddr));
+    nl->sockaddr.nl_family = AF_NETLINK;
+    nl->sockaddr.nl_groups = groups;
+    nl->socklen = sizeof(nl->sockaddr);
+
+    nl->seqno = time(NULL);
+
+    rc = bind(nl->sock, (struct sockaddr *)&nl->sockaddr, nl->socklen);
+    if(rc < 0)
+        goto fail;
+
+    rc = getsockname(nl->sock, (struct sockaddr *)&nl->sockaddr, &nl->socklen);
+
+    if(rc < 0)
+        goto fail;
+
+    return 0;
+
+ fail:
+    {
+        int saved_errno = errno;
+        close(nl->sock);
+        nl->sock = -1;
+        errno = saved_errno;
+        return -1;
+    }
+}
+
+static int
+netlink_read(int (*filter)(struct nlmsghdr *, void *data), void *data)
+{
+
+    int err;
+    struct sockaddr_nl nladdr;
+    struct msghdr msg;
+    struct iovec iov;
+    struct nlmsghdr *nh;
+    int len;
+
+    char buf[8192];
+
+    if(nl_command.sock < 0) {
+        fprintf(stderr,"netlink_read: netlink not initialized.\n");
+        return -1;
+    }
+
+    memset(&nladdr, 0, sizeof(nladdr));
+    nladdr.nl_family = AF_NETLINK;
+
+    memset(&msg, 0, sizeof(msg));
+    msg.msg_name = &nladdr;
+    msg.msg_namelen = sizeof(nladdr);
+    msg.msg_iov = &iov;
+    msg.msg_iovlen = 1;
+
+    iov.iov_base = &buf;
+
+    while(1) {
+        int i = 1;
+
+        iov.iov_len = sizeof(buf);
+        len = recvmsg(nl_command.sock, &msg, 0);
+
+        debugf("netlink_read: recvmsg\n");
+
+        if(len < 0) {
+            if(errno == EINTR)
+                continue;
+            perror("recvmsg(nl_command)");
+            continue;
+        } else if(len == 0) {
+            fprintf(stderr, "EOF on netlink\n");
+            return -1;
+        } else if(msg.msg_namelen != nl_command.socklen) {
+            fprintf(stderr, "sender address length == %d\n", msg.msg_namelen);
+            return -1;
+        } else if(nladdr.nl_pid != 0) {
+            debugf("netlink_read: nl_pid != 0");
+            continue;
+        }
+        for(nh = (struct nlmsghdr *)buf;
+             NLMSG_OK(nh, len);
+             nh = NLMSG_NEXT(nh, len)) {
+            debugf("netlink_read: nh: %d (multi : %s) : ", i++,
+                   (nh->nlmsg_flags & NLM_F_MULTI) ? "yes" : "no");
+            if(nh->nlmsg_pid != nl_command.sockaddr.nl_pid ||
+                nh->nlmsg_seq != nl_command.seqno) {
+                debugf("netlink_read: wrong seqno/pid\n");
+                continue; // or break ? -> read next 'msg'
+            } else if(nh->nlmsg_type == NLMSG_DONE) {
+                debugf("netlink_read: DONE\n");
+                return 0;
+            } else if(nh->nlmsg_type == NLMSG_ERROR) {
+                struct nlmsgerr *err = (struct nlmsgerr *)NLMSG_DATA(nh);
+                if(err->error == 0) {
+                    debugf("netlink_read: ACK\n");
+                    if(!(nh->nlmsg_flags & NLM_F_MULTI))
+                        return 0;
+                    continue;
+                }
+
+                errno = -err->error;
+                perror("netlink_read");
+                return -1;
+            }
+
+            debugf("netlink_read: FILTER\n");
+
+            if(filter) {
+                err = filter(nh, data);
+                if(err || !(nh->nlmsg_flags & NLM_F_MULTI))
+                    return err;
+            }
+
+        }
+        if(msg.msg_flags & MSG_TRUNC) {
+            fprintf(stderr, "Message truncated\n");
+            continue;
+        }
+    }
+
+    return 0;
+}
+
+static int
+netlink_talk(struct nlmsghdr *nh)
+{
+
+    int rc;
+    struct sockaddr_nl nladdr;
+    struct msghdr msg;
+    struct iovec iov;
+
+    if(nl_command.sock < 0) {
+        fprintf(stderr,"netlink_talk: netlink not initialized.\n");
+        return -1;
+    }
+
+    memset(&nladdr, 0, sizeof(nladdr));
+    nladdr.nl_family = AF_NETLINK;
+    nladdr.nl_pid = 0;
+
+    memset(&msg, 0, sizeof(msg));
+    msg.msg_name = &nladdr;
+    msg.msg_namelen = sizeof(nladdr);
+    msg.msg_iov = &iov;
+    msg.msg_iovlen = 1;
+
+    iov.iov_base = nh;
+    iov.iov_len = nh->nlmsg_len;
+
+    nh->nlmsg_flags |= NLM_F_ACK;
+    nh->nlmsg_seq = ++nl_command.seqno;
+
+    rc = sendmsg(nl_command.sock, &msg, 0);
+    if(rc < nh->nlmsg_len) {
+        perror("sendmsg");
+        return -1;
+    }
+
+    return netlink_read(NULL,NULL); /* ACK */
+}
+
+static int
+netlink_send_dump(int type, void *data, int len) {
+
+    struct sockaddr_nl nladdr;
+    struct msghdr msg;
+    struct iovec iov[2];
+    union {
+        char raw[NLMSG_ALIGN(sizeof(struct nlmsghdr))];
+        struct nlmsghdr nh;
+    } buf;
+    int rc;
+
+    if(nl_command.sock < 0) {
+        fprintf(stderr,"netlink_send_dump: netlink not initialized.\n");
+        return -1;
+    }
+
+    /* At least we should send an 'struct rtgenmsg' */
+    if(data == NULL || len == 0)
+        return -1;
+
+    /* And more : using anything else that 'struct rtgenmsg' is currently */
+    /* ignored by the linux kernel (today: 2.6.21) because NLM_F_MATCH is */
+    /* not yet implemented */
+
+    memset(&nladdr, 0, sizeof(nladdr));
+    nladdr.nl_family = AF_NETLINK;
+
+    memset(&msg, 0, sizeof(msg));
+    msg.msg_name = &nladdr;
+    msg.msg_namelen = sizeof(nladdr);
+    msg.msg_iov = iov;
+    msg.msg_iovlen = 2;
+
+    iov[0].iov_base = buf.raw;
+    iov[0].iov_len = sizeof(buf.raw);
+    iov[1].iov_base = data;
+    iov[1].iov_len = len;
+
+    memset(buf.raw, 0, sizeof(buf.raw));
+    buf.nh.nlmsg_flags = NLM_F_DUMP | NLM_F_REQUEST;
+    buf.nh.nlmsg_type = type;
+    buf.nh.nlmsg_seq = ++nl_command.seqno;
+    buf.nh.nlmsg_len = NLMSG_LENGTH(len);
+
+    rc = sendmsg(nl_command.sock, &msg, 0);
+    if(rc < buf.nh.nlmsg_len) {
+        perror("sendmsg");
+        return -1;
+    }
+
+    return 0;
+}
+
+static int
+netlink_listen(int (*monitor)(struct nlmsghdr *nh, void *data), void *data) {
+
+    int err;
+    struct msghdr msg;
+    struct sockaddr_nl nladdr;
+    struct iovec iov;
+    struct nlmsghdr *nh;
+    int len, i;
+
+    static char buf[8192];
+
+    if(nl_listen.sock < 0) {
+        fprintf(stderr,"netlink_listen: netlink not initialized.\n");
+        return -1;
+    }
+
+    memset(&msg, 0, sizeof(msg));
+    msg.msg_name = &nladdr;
+    msg.msg_namelen = sizeof(nladdr);
+    msg.msg_iov = &iov;
+    msg.msg_iovlen = 1;
+
+    memset(&nladdr, 0, sizeof(nladdr));
+    nladdr.nl_family = AF_NETLINK;
+
+    iov.iov_base = buf;
+    iov.iov_len = sizeof(buf);
+
+    // TODO non block ??
+    len = recvmsg(nl_listen.sock, &msg, 0);
+
+    debugf("netlink_listen: recvmsg\n");
+
+    if(len < 0) {
+        if(errno == EINTR)
+            return 0;
+        perror("OVERRUN");
+        return -1;
+    }
+
+    if(len == 0) {
+        fprintf(stderr, "netlink_listen: EOF on netlink\n");
+        return -1;
+    }
+
+    if(msg.msg_namelen != nl_listen.socklen) {
+        fprintf(stderr, "netlink_listen: sender address length == %d\n",
+                msg.msg_namelen);
+        return -1;
+    }
+
+    for(nh = (struct nlmsghdr *)buf, i = 1; NLMSG_OK(nh, len);
+        nh = NLMSG_NEXT(nh, len), i++) {
+
+        debugf("netlink_listen: nh: %d (multi : %s) : ", i,
+               (nh->nlmsg_flags & NLM_F_MULTI) ? "yes" : "no");
+
+        if(nh->nlmsg_type == NLMSG_DONE) {
+            debugf("netlink_listen: DONE\n");
+            continue;
+        }
+
+        if(nh->nlmsg_type == NLMSG_ERROR) {
+            debugf("netlink_listen: ERROR\n");
+            continue;
+        }
+
+        /* Ignore les messages venant de babel */
+        if(nh->nlmsg_pid == nl_command.sockaddr.nl_pid)
+            continue;
+
+        if(monitor) {
+            err = monitor(nh,data);
+            if(err) return err;
+        }
+
+    }
+    if(msg.msg_flags & MSG_TRUNC) {
+        fprintf(stderr, "netlink_listen: Message truncated\n");
+    }
+    return 0;
+}
+
 int
 kernel_setup(int setup)
 {
     int rc;
 
     if(setup) {
+        rc = netlink_socket(&nl_command, 0);
+        if(rc < 0) {
+            perror("netlink_socket(0)");
+            return -1;
+        }
+
+        rc = netlink_socket(&nl_listen, RTMGRP_IPV6_ROUTE);
+        if(rc < 0) {
+            perror("netlink_socket(RTMGRP_IPV6_ROUTE)");
+            return -1;
+        }
+
         old_forwarding = read_proc("/proc/sys/net/ipv6/conf/all/forwarding");
         if(old_forwarding < 0) {
             perror("Couldn't read forwarding knob.");
@@ -136,7 +475,15 @@ kernel_setup(int setup)
                 return -1;
             }
         }
+
+        close(nl_command.sock);
+        nl_command.sock = -1;
+
+        close(nl_listen.sock);
+        nl_listen.sock = -1;
+
         return 1;
+
     }
 }
 
@@ -197,20 +544,20 @@ kernel_interface_wireless(const char *ifname, int ifindex)
     return rc;
 }
 
-static int route_socket = -1;
-
 int
 kernel_route(int add, const unsigned char *dest, unsigned short plen,
              const unsigned char *gate, int ifindex, unsigned int metric)
 {
-    struct in6_rtmsg msg;
-    int rc;
 
-    if(route_socket < 0) {
-        route_socket = socket(AF_INET6, SOCK_DGRAM, 0);
-        if(route_socket < 0)
-            return -1;
-    }
+    union { char raw[1024]; struct nlmsghdr nh; } buf;
+    struct rtmsg *rtm;
+    struct rtattr *rta;
+    int len = sizeof(buf.raw);
+
+    debugf("kernel_route: %s: %s/%d metric %d via %d nexthop %s\n",
+           add ? "Add" : "Del",
+           format_address(dest), plen, metric, ifindex,
+           format_address(gate));
 
     if(ifindex_lo < 0) {
         ifindex_lo = if_nametoindex("lo");
@@ -218,82 +565,220 @@ kernel_route(int add, const unsigned char *dest, unsigned short plen,
             return -1;
     }
 
-    memset(&msg, 0, sizeof(msg));
-
-    msg.rtmsg_flags = RTF_UP;
-    memcpy(&msg.rtmsg_dst, dest, sizeof(struct in6_addr));
-    msg.rtmsg_dst_len = plen;
-    msg.rtmsg_metric = metric;
-
-    if(plen >= 128)
-        msg.rtmsg_flags |= RTF_HOST;
-
-    if(metric >= KERNEL_INFINITY) {
-        msg.rtmsg_ifindex = ifindex_lo;
+    memset(buf.raw, 0, sizeof(buf.raw));
+    if(add) {
+        buf.nh.nlmsg_flags = NLM_F_REQUEST | NLM_F_CREATE | NLM_F_EXCL;
+        buf.nh.nlmsg_type = RTM_NEWROUTE;
     } else {
-        msg.rtmsg_ifindex = ifindex;
-        if(plen < 128 || memcmp(dest, gate, 16) != 0)
-            msg.rtmsg_flags |= RTF_GATEWAY;
+        buf.nh.nlmsg_flags = NLM_F_REQUEST;
+        buf.nh.nlmsg_type = RTM_DELROUTE;
     }
 
-    memcpy(&msg.rtmsg_gateway, gate, sizeof(struct in6_addr));
+    rtm = NLMSG_DATA(&buf.nh);
+    rtm->rtm_family = AF_INET6;
+    rtm->rtm_dst_len = plen;
+    rtm->rtm_table = RT_TABLE_MAIN;
+    rtm->rtm_scope = RT_SCOPE_LINK;
+    if(metric < KERNEL_INFINITY)
+        rtm->rtm_type = RTN_UNICAST;
+    else
+        rtm->rtm_type = RTN_UNREACHABLE;
+    rtm->rtm_protocol = RTPROT_BABEL;
 
-    rc = ioctl(route_socket, add ? SIOCADDRT : SIOCDELRT, &msg);
-    if(rc < 0)
-        return -1;
-    return 1;
+    rta = RTM_RTA(rtm);
+
+    rta = RTA_NEXT(rta, len);
+    rta->rta_len = RTA_LENGTH(sizeof(struct in6_addr));
+    rta->rta_type = RTA_DST;
+    memcpy(RTA_DATA(rta), dest, sizeof(struct in6_addr));
+
+    rta = RTA_NEXT(rta, len);
+    rta->rta_len = RTA_LENGTH(sizeof(int));
+    rta->rta_type = RTA_PRIORITY;
+
+    if(metric < KERNEL_INFINITY) {
+        *(int*)RTA_DATA(rta) = metric;
+        rta = RTA_NEXT(rta, len);
+        rta->rta_len = RTA_LENGTH(sizeof(int));
+        rta->rta_type = RTA_OIF;
+        *(int*)RTA_DATA(rta) = ifindex;
+
+        rta = RTA_NEXT(rta, len);
+        rta->rta_len = RTA_LENGTH(sizeof(struct in6_addr));
+        rta->rta_type = RTA_GATEWAY;
+        memcpy(RTA_DATA(rta), gate, sizeof(struct in6_addr));
+    } else {
+        *(int*)RTA_DATA(rta) = -1;
+    }
+    buf.nh.nlmsg_len = (char*)rta + rta->rta_len - buf.raw;
+
+    return netlink_talk(&buf.nh);
 }
 
-/* This function should not return routes installed by us.  It currently
-   does, which could lead to routing loops in some cases. */
+static int
+parse_kernel_route_rta(struct rtmsg *rtm, int len,
+                       struct kernel_route *route, int *table)
+{
+
+    struct rtattr *rta= RTM_RTA(rtm);;
+    len -= NLMSG_ALIGN(sizeof(*rtm));
+
+    memset(&route->prefix,0,sizeof(struct in6_addr));
+    memset(&route->gw,0,sizeof(struct in6_addr));
+    route->plen = rtm->rtm_dst_len;
+    route->metric = KERNEL_INFINITY;
+    route->ifindex = 0;
+
+    *table = rtm->rtm_table;
+
+    while(RTA_OK(rta, len)) {
+        switch (rta->rta_type) {
+        case RTA_DST:
+            memcpy(&route->prefix,RTA_DATA(rta),16);
+            break;
+        case RTA_GATEWAY:
+            memcpy(&route->gw,RTA_DATA(rta),16);
+            break;
+        case RTA_OIF:
+            route->ifindex = *(int*)RTA_DATA(rta);
+            break;
+        case RTA_PRIORITY:
+            route->metric = *(int*)RTA_DATA(rta);
+            break;
+#ifdef RTA_TABLE
+       case RTA_TABLE:
+            *table = *(int*)RTA_DATA(rta);
+            break;
+#endif
+        default:
+            break;
+        }
+        rta = RTA_NEXT(rta, len);
+    }
+
+    route->metric = MIN(route->metric, KERNEL_INFINITY);
+    // TODO Error handling ?? len != 0
+    return 0;
+
+}
+
+static void
+print_kernel_route(int add, int protocol, int type,
+                   struct kernel_route *route)
+{
+    char ifname[IFNAMSIZ];
+    char addr_prefix[INET6_ADDRSTRLEN];
+    char addr_gw[INET6_ADDRSTRLEN];
+
+    debugf("%sing route: ",
+           add == RTM_NEWROUTE ? "Add" : "Delet");
+
+    if(!inet_ntop(AF_INET6, route->prefix,
+                  addr_prefix, sizeof(addr_prefix)) ||
+       !inet_ntop(AF_INET6,route->gw, addr_gw, sizeof(addr_gw)) ||
+       !if_indextoname(route->ifindex, ifname)) {
+        debugf("failed !");
+        return;
+    }
+
+    debugf("dest: %s/%d\tgw: %s\tmetric: %d\tif: %s\t",
+           addr_prefix, route->plen, addr_gw, route->metric, ifname);
+    debugf("(proto: %d, type: %d)\n", protocol, type);
+
+}
+
+static int
+monitor_kernel_route(struct nlmsghdr *nh, void *data)
+{
+
+    struct kernel_route route;
+    int table;
+
+    int len = nh->nlmsg_len;
+    struct rtmsg *rtm;
+
+    if(nh->nlmsg_type != RTM_NEWROUTE && nh->nlmsg_type != RTM_DELROUTE)
+        return 0;
+
+    rtm = (struct rtmsg*)NLMSG_DATA(nh);
+    len -= NLMSG_LENGTH(0);
+
+    parse_kernel_route_rta(rtm, len, &route, &table);
+    print_kernel_route(nh->nlmsg_type, rtm->rtm_protocol,
+                       rtm->rtm_type, &route);
+    return 0;
+}
+
+static int
+filter_kernel_routes(struct nlmsghdr *nh, void *data)
+{
+
+    void **args = (void**)data;
+    int maxplen = *(int*)args[0];
+    int maxroutes = *(int*)args[1];
+    struct kernel_route *routes = (struct kernel_route *)args[2];
+    int *found = (int*)args[3];
+
+    struct rtmsg *rtm;
+    int table;
+
+    int len = nh->nlmsg_len;
+
+    if(*found >= maxroutes)
+        return 0;
+
+    if(nh->nlmsg_type != RTM_NEWROUTE)
+        return 0;
+    rtm = (struct rtmsg*)NLMSG_DATA(nh);
+    len -= NLMSG_LENGTH(0);
+
+    if(rtm->rtm_protocol == RTPROT_BABEL)
+        return 0;
+
+    if(rtm->rtm_dst_len > maxplen || rtm->rtm_src_len != 0)
+        return 0;
+
+    parse_kernel_route_rta(rtm, len, &routes[*found], &table);
+
+    if(table != RT_TABLE_MAIN)
+        return 0;
+
+    //*found++; ??
+    *found = (*found)+1;
+
+    return 0;
+
+}
+
+/* This function should not return routes installed by us. */
 int
 kernel_routes(int maxplen, struct kernel_route *routes, int maxroutes)
 {
-    FILE *f;
-    char dst[33], src[33], gw[33], iface[IF_NAMESIZE];
-    unsigned int dst_plen, src_plen, metric, use, refcnt, flags;
-    int n, rc;
+    int rc;
+    int maxp = maxplen;
+    int maxr = maxroutes;
+    int found = 0;
+    void *data[4] = { &maxp, &maxr, routes, &found };
+    struct rtgenmsg g;
 
-    f = fopen("/proc/net/ipv6_route", "r");
-    if(f == NULL)
+    memset(&g, 0, sizeof(g));
+    g.rtgen_family = AF_INET6;
+    rc = netlink_send_dump(RTM_GETROUTE, &g, sizeof(g));
+    if(rc < 0)
         return -1;
 
-    n = 0;
+    rc = netlink_read(filter_kernel_routes, (void*)data);
+    if(rc < 0)
+        return -1;
 
-    while(n < maxroutes) {
-        rc = fscanf(f, "%32s %02x %32s %02x %32s %08x %08x %08x %08x %s",
-                     dst, &dst_plen, src, &src_plen, gw,
-                     &metric, &use, &refcnt, &flags, iface);
-        if(rc != 10)
-            break;
-
-        if(!(flags & RTF_UP) || dst_plen > maxplen || src_plen != 0)
-            goto skip;
-
-        rc = parse_address(dst, routes[n].prefix);
-        if(rc < 0)
-            goto skip;
-
-        routes[n].plen = dst_plen;
-        routes[n].metric = MIN(metric, (unsigned)KERNEL_INFINITY);
-        routes[n].ifindex = if_nametoindex(iface);
-        if(routes[n].ifindex < 0)
-            goto skip;
-
-        if(flags & RTF_GATEWAY)
-            rc = parse_address(gw, routes[n].gw);
-        else
-            memset(routes[n].gw, 0, 16);
-    skip:
-        n++;
-    }
-
-    fclose(f);
-    return n;
+    return found;
 }
 
 int
 kernel_callback()
 {
-    return 0;
+    if(nl_listen.sock < 0)
+        return -1;
+
+    return netlink_listen(monitor_kernel_route, NULL);
 }
