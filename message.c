45939d213ab4b291af063895a87dd9de276c9ee4
Juliusz Chroboczek <jch@pps.jussieu.fr>
2007-09-27 04:39:18 +0200

    Fix incorrect test for overflow in ihu_interval.

--- message.c
+++ message.c
@@ -675,7 +675,7 @@ send_ihu(struct neighbour *neigh, struct network *net)
         return;
     }
 
-    if(net && net->ihu_interval * 100 >= 0xFFFF)
+    if(net && net->ihu_interval * 100 <= 0xFFFF)
         interval = net->ihu_interval * 100;
     else
         interval = 0;
