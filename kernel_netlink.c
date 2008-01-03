f9a4f4594583af3ce83d59fe10a6561f83e95404
Juliusz Chroboczek <jch@pps.jussieu.fr>
2008-01-03 11:48:12 +0100

    Update copyright message.

--- kernel_netlink.c
+++ kernel_netlink.c
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2007 by Grégoire Henry, Julien Cristau and Juliusz Chroboczek
+Copyright 2007, 2008 by Grégoire Henry, Julien Cristau and Juliusz Chroboczek
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
