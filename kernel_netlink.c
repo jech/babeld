50c112c7425c1ff54c8ba506e9a3025f2d71af2c
Juliusz Chroboczek <jch@pps.jussieu.fr>
2008-05-24 01:21:01 +0200

    Avoid warning.

--- kernel_netlink.c
+++ kernel_netlink.c
@@ -785,7 +785,7 @@ parse_kernel_route_rta(struct rtmsg *rtm, int len, struct kernel_route *route)
     memset(&route->gw, 0, sizeof(struct in6_addr));
     route->plen = rtm->rtm_dst_len;
     if(rtm->rtm_family == AF_INET) {
-        const char zeroes[4] = {0, 0, 0, 0};
+        const unsigned char zeroes[4] = {0, 0, 0, 0};
         v4tov6(route->prefix, zeroes);
         route->plen += 96;
     }
