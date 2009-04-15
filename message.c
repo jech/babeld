e4d782bac8bc4391c5196692c22f3583ee426e8a
Juliusz Chroboczek <jch@pps.jussieu.fr>
2009-04-15 04:04:41 +0200

    Send retractions even when there is no matching route.

--- message.c
+++ message.c
@@ -905,6 +905,11 @@ flushupdates(struct network *net)
                 last_prefix = route->src->prefix;
                 last_plen = route->src->plen;
                 continue;
+            } else {
+                /* check_xroutes sends updates after retracting an xroute,
+                   so send a retraction if there's no matching route. */
+                really_send_update(net, myid, b[i].prefix, b[i].plen,
+                                   myseqno, INFINITY);
             }
         }
         schedule_flush_now(net);
