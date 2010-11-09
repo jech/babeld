e59fa3827bac7d0f12fd7b4925f40ec7b2e6ed07
dermiste <dermiste@screwball-coders.net>
2010-11-09 13:18:16 +0100

    Fix compilation warnings on OpenBSD

--- net.c
+++ net.c
@@ -27,8 +27,8 @@ THE SOFTWARE.
 #include <sys/types.h>
 #include <sys/uio.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <errno.h>
 
 #include "babeld.h"
