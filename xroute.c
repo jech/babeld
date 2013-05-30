d94977c9ac632ebf7997f5e5768ae0698fb8663d
Matthieu Boutier <boutier@pps.univ-paris-diderot.fr>
2013-05-30 11:52:45 +0200

    Redistribute in-ss-prefix dest with default source-prefix.
    If a kernel route has its destination in our source prefix, it
    will be redistribute with the default source prefix.

--- xroute.c
+++ xroute.c
@@ -199,6 +199,7 @@ check_xroutes(int send_updates)
     for (i = 0; i < numroutes;) {
         const unsigned char *ss_prefix;
         unsigned char ss_plen;
+        enum prefixes_status dst_st;
         if (v4mapped(routes[i].prefix)) {
             ss_prefix = source_specific_addr;
             ss_plen   = source_specific_plen;
@@ -215,8 +216,12 @@ check_xroutes(int send_updates)
                 numroutes--; /* no i ++ */
                 break;
             case PST_LESS_SPECIFIC:
-                memcpy(routes[i].src_prefix, ss_prefix, 16);
-                routes[i].src_plen = ss_plen;
+                dst_st = prefixes_cmp(routes[i].prefix, routes[i].plen,
+                                      ss_prefix, ss_plen);
+                if (!(dst_st == PST_MORE_SPECIFIC || dst_st == PST_EQUALS)) {
+                    memcpy(routes[i].src_prefix, ss_prefix, 16);
+                    routes[i].src_plen = ss_plen;
+                }
                 /* fall through */;
             case PST_EQUALS:
             case PST_MORE_SPECIFIC:
