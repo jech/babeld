4b5b319f7618dd39e4540d5f6a4098b1dbe96a95
Baptiste Jonglez <baptiste.jonglez@ens-lyon.fr>
2013-06-18 18:43:58 +0200

    Make 'rtt_exponential_decay' relative to an interface

--- babeld.h
+++ babeld.h
@@ -90,7 +90,6 @@ extern int do_daemonise;
 extern const char *logfile, *pidfile, *state_file;
 extern int link_detect;
 extern int all_wireless;
-extern unsigned int rtt_exponential_decay;
 
 extern unsigned char myid[8];
 
