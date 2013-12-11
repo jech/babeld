e399c486fa20206796d3092f0b59d3fe1858346e
Matthieu Boutier <boutier@pps.univ-paris-diderot.fr>
2013-12-11 14:19:02 +0100

    Fix bug: accept routes with source lengths bigger than 0.

--- kernel_netlink.c
+++ kernel_netlink.c
@@ -1223,9 +1223,6 @@ filter_kernel_routes(struct nlmsghdr *nh, void *data)
     if(rtm->rtm_protocol == RTPROT_BABEL)
         return 0;
 
-    if(rtm->rtm_src_len != 0)
-        return 0;
-
     /* Ignore cached routes, advertised by some kernels (linux 3.x). */
     if(rtm->rtm_flags & RTM_F_CLONED)
         return 0;
