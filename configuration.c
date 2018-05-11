jech/babeld

commit 4f4e3cba796e719b97449f6303961171132f3c74

Fix parsing of source prefix length in filters for IPv4 routes.

configuration.c
@@ -451,8 +451,6 @@ parse_filter(int c, gnc_t gnc, void *closure, struct filter **filter_return)
                 filter->af = af;
             else if(filter->af != af)
                 goto error;
-            if(af == AF_INET && filter->action.src_plen == 96)
-                memset(&filter->action.src_prefix, 0, 16);
         } else if(strcmp(token, "table") == 0) {
             int table;
             c = getint(c, &table, gnc, closure);
@@ -472,6 +470,8 @@ parse_filter(int c, gnc_t gnc, void *closure, struct filter **filter_return)
     } else if(filter->af == AF_INET) {
         filter->plen_le += 96;
         filter->plen_ge += 96;
+        filter->src_plen_le += 96;
+        filter->src_plen_ge += 96;
     }
     *filter_return = filter;
     return c;

commit 1ffff57e44a4a507786f5626070d9b1f3e371389

Fix parsing of source length in filters.
This fixes a bug that was introduced in commit 4f4e3cb, and prevented
non-source-specific IPv4 routes from being redistributed.  Thanks
to Niklas Yann Wettengel for the detective work.

configuration.c
@@ -479,10 +479,14 @@ parse_filter(int c, gnc_t gnc, void *closure, struct filter **filter_return)
            filter->src_plen_le < 128 || filter->src_plen_ge > 0)
             filter->af = AF_INET6;
     } else if(filter->af == AF_INET) {
-        filter->plen_le += 96;
-        filter->plen_ge += 96;
-        filter->src_plen_le += 96;
-        filter->src_plen_ge += 96;
+        if(filter->plen_le < 128)
+            filter->plen_le += 96;
+        if(filter->plen_ge > 0)
+            filter->plen_ge += 96;
+        if(filter->src_plen_le < 128)
+            filter->src_plen_le += 96;
+        if(filter->src_plen_ge > 0)
+            filter->src_plen_ge += 96;
     }
     *filter_return = filter;
     return c;