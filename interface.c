a62b7c9b6dbab378d69a523a10a9de33c2624fbb
Etienne MARAIS <etienne@marais.green>
2019-06-24 11:12:50 +0200

    Add no_hmac_verify flag.
    Flag to allow empty or wrong hmac in the packet trailer.

--- interface.c
+++ interface.c
@@ -396,7 +396,8 @@ interface_updown(struct interface *ifp, int up)
 
         if(IF_CONF(ifp, unicast) == CONFIG_YES)
             ifp->flags |= IF_UNICAST;
-
+        if(IF_CONF(ifp, no_hmac_verify) == CONFIG_YES)
+            ifp->flags |= IF_NO_HMAC_VERIFY;
         if(IF_CONF(ifp, hello_interval) > 0)
             ifp->hello_interval = IF_CONF(ifp, hello_interval);
         else if(type == IF_TYPE_WIRELESS)
