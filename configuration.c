5c12ead3517aaddff33f3e8c6af27294a516c503
Antonin Décimo <antonin.decimo@gmail.com>
2019-06-28 11:54:48 +0200

    Fix dereference of a null pointer.
    `if_conf` can be null if the allocation fails in line 520.

--- configuration.c
+++ configuration.c
@@ -657,7 +657,7 @@ parse_anonymous_ifconf(int c, gnc_t gnc, void *closure,
     return c;
 
  error:
-    if(if_conf->ifname)
+    if(if_conf)
         free(if_conf->ifname);
     free(if_conf);
     return -2;
