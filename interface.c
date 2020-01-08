3cb0ab7a491b376913f2f6a40c810dc7cabc0037
Antonin Décimo <antonin.decimo@gmail.com>
2020-01-08 17:18:04 +0100

    Use RFC3542 for IPv6 on macOS.

--- interface.c
+++ interface.c
@@ -20,6 +20,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
+#define __APPLE_USE_RFC_3542
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
