2e43bd675c3ce1f2ea1fc3b8c933e850d6733319
Antonin Décimo <antonin.decimo@gmail.com>
2020-06-18 20:57:05 +0200

    Rework h2i.

--- util.c
+++ util.c
@@ -197,14 +197,14 @@ parse_thousands(const char *string)
     return -1;
 }
 
-static int
+ATTRIBUTE ((const)) static int
 h2i(char c)
 {
-    if(c >= '0' && c <= '9')
+    if('0' <= c && c <= '9')
         return c - '0';
-    else if(c >= 'a' && c <= 'f')
+    else if('a' <= c && c <= 'f')
         return c - 'a' + 10;
-    else if(c >= 'A' && c <= 'F')
+    else if('A' <= c && c <= 'F')
         return c - 'A' + 10;
     else
         return -1;
