e83d26e46e40425ea3e28b89c40935e6fad35bfb
tomaz1502 <tomgm1502@gmail.com>
2024-06-04 18:03:41 +0200

    Expose functions to be tested in route.c.

--- route.c
+++ route.c
@@ -41,19 +41,20 @@ THE SOFTWARE.
 #include "local.h"
 
 struct babel_route **routes = NULL;
-static int route_slots = 0, max_route_slots = 0;
+static int max_route_slots = 0;
+int route_slots = 0;
 int kernel_metric = 0, reflect_kernel_metric = 0;
 int allow_duplicates = -1;
 int diversity_factor = 256;     /* in units of 1/256 */
 
-static int smoothing_half_life = 0;
-static int two_to_the_one_over_hl = 0; /* 2^(1/hl) * 0x10000 */
+int smoothing_half_life = 0;
+int two_to_the_one_over_hl = 0; /* 2^(1/hl) * 0x10000 */
 
 /* We maintain a list of "slots", ordered by prefix.  Every slot
    contains a linked list of the routes to this prefix, with the
    installed route, if any, at the head of the list. */
 
-static int
+int
 route_compare(const unsigned char *prefix, unsigned char plen,
               const unsigned char *src_prefix, unsigned char src_plen,
               struct babel_route *route)
@@ -94,7 +95,7 @@ route_compare(const unsigned char *prefix, unsigned char plen,
 /* Performs binary search, returns -1 in case of failure.  In the latter
    case, new_return is the place where to insert the new element. */
 
-static int
+int
 find_route_slot(const unsigned char *prefix, unsigned char plen,
                 const unsigned char *src_prefix, unsigned char src_plen,
                 int *new_return)
@@ -189,7 +190,7 @@ resize_route_table(int new_slots)
 
 /* Insert a route into the table.  If successful, retains the route.
    On failure, caller must free the route. */
-static struct babel_route *
+struct babel_route *
 insert_route(struct babel_route *route)
 {
     int i, n;
@@ -405,7 +406,7 @@ route_stream_done(struct route_stream *stream)
     free(stream);
 }
 
-static int
+int
 metric_to_kernel(int metric)
 {
         if(metric >= INFINITY) {
@@ -566,7 +567,7 @@ switch_routes(struct babel_route *old, struct babel_route *new)
     local_notify_route(new, LOCAL_CHANGE);
 }
 
-static void
+void
 change_route_metric(struct babel_route *route,
                     unsigned refmetric, unsigned cost, unsigned add)
 {
